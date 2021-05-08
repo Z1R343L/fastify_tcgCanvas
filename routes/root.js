9f0f886b81e8cf7405d784d25063ac732dc869f1
Z1R343L <jan2705g@gmail.com>
2021-05-08 07:38:16 +0200

    case- fixes (code style)

--- routes/root.js
+++ routes/root.js
@@ -2,10 +2,10 @@
 
 const { resolveImage, Canvas } = require("canvas-constructor");
 
-const card_hight = 342;
-const card_width = 245;
+const cardHight = 342;
+const cardWidth = 245;
 const cards_url = 'https://images.pokemontcg.io/';
-const dummy_card = 'basep-38';
+const dummyCard = 'basep-38';
 const def_bg = 'https://cdn.discordapp.com/attachments/745028626206031973/840358497110589440/TmNB9Rj.jpg';
 
 function resolveCard(cId) {
@@ -18,16 +18,16 @@ async function drawTable(rq)  {
   let col; if (!rq.col) {col = 5} else {col=rq.col}
   let padding; if (!rq.padding) {padding=15} else {padding=parseInt(rq.padding)}
   let bguri; if(!rq.bg) {bguri = def_bg;} else {bguri = rq.bg}
-  let card0; if (!rq.c0) {card0 = dummy_card;} else {card0 = rq.c0;}
-  let card1; if (!rq.c1) {card1 = dummy_card;} else {card1 = rq.c1;}
-  let card2; if (!rq.c2) {card2 = dummy_card;} else {card2 = rq.c2;}
-  let card3; if (!rq.c3) {card3 = dummy_card;} else {card3 = rq.c3;}
-  let card4; if (!rq.c4) {card4 = dummy_card;} else {card4 = rq.c4;}
-  let card5; if (!rq.c5) {card5 = dummy_card;} else {card5 = rq.c5;}
-  let card6; if (!rq.c6) {card6 = dummy_card;} else {card6 = rq.c6;}
-  let card7; if (!rq.c7) {card7 = dummy_card;} else {card7 = rq.c7;}
-  let card8; if (!rq.c8) {card8 = dummy_card;} else {card8 = rq.c8;}
-  let card9; if (!rq.c9) {card9 = dummy_card;} else {card9 = rq.c9;}
+  let card0; if (!rq.c0) {card0 = dummyCard;} else {card0 = rq.c0;}
+  let card1; if (!rq.c1) {card1 = dummyCard;} else {card1 = rq.c1;}
+  let card2; if (!rq.c2) {card2 = dummyCard;} else {card2 = rq.c2;}
+  let card3; if (!rq.c3) {card3 = dummyCard;} else {card3 = rq.c3;}
+  let card4; if (!rq.c4) {card4 = dummyCard;} else {card4 = rq.c4;}
+  let card5; if (!rq.c5) {card5 = dummyCard;} else {card5 = rq.c5;}
+  let card6; if (!rq.c6) {card6 = dummyCard;} else {card6 = rq.c6;}
+  let card7; if (!rq.c7) {card7 = dummyCard;} else {card7 = rq.c7;}
+  let card8; if (!rq.c8) {card8 = dummyCard;} else {card8 = rq.c8;}
+  let card9; if (!rq.c9) {card9 = dummyCard;} else {card9 = rq.c9;}
   const bgi = await resolveImage(bguri);
   const ci0 = await resolveImage(resolveCard(card0));
   const ci1 = await resolveImage(resolveCard(card1));
@@ -40,13 +40,13 @@ async function drawTable(rq)  {
   const ci8 = await resolveImage(resolveCard(card8));
   const ci9 = await resolveImage(resolveCard(card9));
   const lin1 = padding;
-  const lin2 = lin1 + padding + card_hight;
-  const lin3 = lin2 + padding + card_hight;
+  const lin2 = lin1 + padding + cardHight;
+  const lin3 = lin2 + padding + cardHight;
   const col1 = padding;
-  const col2 = col1 + padding + card_width;
-  const col3 = col2 + padding + card_width;
-  const col4 = col3 + padding + card_width;
-  const col5 = col4 + padding + card_width;
+  const col2 = col1 + padding + cardWidth;
+  const col3 = col2 + padding + cardWidth;
+  const col4 = col3 + padding + cardWidth;
+  const col5 = col4 + padding + cardWidth;
   let canvas_w;
   let canvas_h;
   let c0col;
@@ -70,8 +70,8 @@ async function drawTable(rq)  {
   let c9col;
   let c9lin;
   if (col === 5) {
-    canvas_w = (card_width*5) + (padding*6);
-    canvas_h = (card_hight*2) + (padding*3);
+    canvas_w = (cardWidth*5) + (padding*6);
+    canvas_h = (cardHight*2) + (padding*3);
     c0col = col1;
     c0lin = lin1
     c1col = col2;
@@ -93,8 +93,8 @@ async function drawTable(rq)  {
     c9col = col5;
     c9lin = lin2;
   } else {
-    canvas_w = (card_width*4) + (padding*5);
-    canvas_h = (card_hight*3) + (padding*4);
+    canvas_w = (cardWidth*4) + (padding*5);
+    canvas_h = (cardHight*3) + (padding*4);
     c0col = col1;
     c0lin = lin1;
     c1col = col2;
